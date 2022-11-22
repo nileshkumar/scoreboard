1eecbe9f361a82a18393e1c342744f2c3c95e90d
Nilesh Kumar <nileshkumar.be@gmail.com>
2022-11-22 05:48:31 +0100

    instantiate more objects

--- main.go
+++ main.go
@@ -6,8 +6,14 @@ import (
 
 func main() {
 	fmt.Println("----Football Scoreboard----")
+	fmt.Println("******Game Started******")
 
 	gamescore1 := newScoreBoard("Germany", "Italy")
+	gamescore2 := newScoreBoard("France", "Portugal")
+	gamescore3 := newScoreBoard("Spain", "Brazil")
+
 	fmt.Println(gamescore1)
+	fmt.Println(gamescore2)
+	fmt.Println(gamescore3)
   
 }
