ccdcccbcb50a342d488f3c2ce478e193c5f35bfd
Nilesh Kumar <nileshkumar.be@gmail.com>
2022-11-22 05:43:22 +0100

    instantiate an object with initial value

--- main.go
+++ main.go
@@ -6,5 +6,8 @@ import (
 
 func main() {
 	fmt.Println("----Football Scoreboard----")
+
+	gamescore1 := newScoreBoard("Germany", "Italy")
+	fmt.Println(gamescore1)
   
 }
--- scoreboard.go
+++ scoreboard.go
@@ -7,3 +7,13 @@ type scoreboard struct {
 	away_team_score int8
 }
 
+func newScoreBoard(home_team string, away_team string) scoreboard {
+  score := scoreboard{
+    home_team: home_team,
+    away_team: away_team,
+    home_team_score: 0,
+    away_team_score: 0,
+  }
+
+  return score
+}
